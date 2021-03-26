f26f0b123bfa3c6e5235a185b06bde19cf63f6cc
MyMac <mymac@Macbook.local>
2021-03-26 13:17:43 +0800

    Calculator refactor

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,41 +1,62 @@
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+
 const readline = require("readline-sync");
 
-console.log("Welcome to Calculator");
+prompt("Welcome to Calculator");
 
-console.log("What is the first number?");
+prompt("What is the first number?");
 let firstNumber = readline.question();
 
+while (invalidNumber(firstNumber)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  firstNumber = readline.question();
+}
 
-console.log("What is the second number?");
+
+prompt("What is the second number?");
 let secondNumber = readline.question();
 
 
-console.log("What operation would you like to perform?");
+while (invalidNumber(secondNumber)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  secondNumber = readline.question();
+}
+
+
+prompt("What operation would you like to perform?");
 console.log("1) Add 2) Subtract 3) Multiply 4) Divide");
 let operation = readline.question();
 
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt("Must choose 1, 2, 3 or 4.");
+  operation = readline.question();
+}
+
+
 let output;
-if (operation === "1") {
-  output = Number(firstNumber) + Number(secondNumber); 
-} else if (operation === "2") {
-  output = Number(firstNumber) - Number(secondNumber); 
-} else if (operation === "3") {
-  output = Number(firstNumber) * Number(secondNumber);
-} else if (operation === "4") {
-  output = Number(firstNumber) / Number(secondNumber);
+switch (operation) {
+  case "1":
+    output = Number(firstNumber) + Number(secondNumber);
+    break;
+  case "2":
+    output = Number(firstNumber) - Number(secondNumber);
+    break;
+  case "3":
+    output = Number(firstNumber) * Number(secondNumber);
+    break;
+  case "4":
+    output = Number(firstNumber) / Number(secondNumber);
+    break;
 }
 
 
 console.log(`The result is ${output}`);
 
-
-
-// get first number
-
-// get second number
-
-// get operation
-
-// perform operation
-
-// print result
---
+++ lesson_2/pseudocode.js
@@ -0,0 +1,24 @@
+// a function that returns the sum of two numbers
+
+/*
+GET number one
+GET number two
+RETURN one + two
+*/
+
+//a function that takes an array of strings, and returns a string that is all those strings concatenated together
+
+/* 
+GET array
+RETURN array.join()
+/*
+
+// a function that takes an array of integers, and returns a new array with every other element
+
+/*
+GET array
+LOOP through array
+SET everyOther[] = every other number
+RETURN everyOther
+
+*/
